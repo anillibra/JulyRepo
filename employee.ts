480e2254d41d905cab329749d78ea783d766cdff
Anil Singh <anillibra76@gmail.com>
2017-07-12 22:19:38 +0530

    This is Final comments of modifiy story

--- employee.ts
+++ employee.ts
@@ -10,4 +10,10 @@ export class Employee {
     add(): void {
         this.employees.push({ _id: 1, name: "Parik" });
     }
+
+    // Added modify function for employees  
+    modifyEmployee(): void {
+        this.employees.splice(0, 1, { _id: 1, name: "Parik Sheetle" });
+        // this employee can't be modified
+    }
 }
